b5ee4b2c5800022893c0e4254254aac80d0b33d9
LuisKrotz <luis.krotz@gmail.com>
2020-10-30 00:23:36 -0300

    LIST: list dragons, edit, remove, create and exclude

--- src/components/DragonList.vue
+++ src/components/DragonList.vue
@@ -1,37 +1,139 @@
 <template>
-  <ul class="hello">
-    <li v-for="dragon in dragons" :key="dragon.id">
-      {{ dragon.name }} / {{ dragon.type }}
-    </li>
-  </ul>
+    <div :class="'max-area ' + (!loaded ? 'loading': '')">
+        <div :class="'dragon-list ' + (!crud ? 'col-2' : 'col-3') ">
+            <div class="dragon-list-item header">
+              <div class="dragon-list-header">Dragon Name</div>
+              <div class="dragon-list-header">Dragon Type</div>
+              <div class="dragon-list-header" v-if="crud">Options</div>
+            </div>
+          <div class="dragon-list-item" v-if="crud">
+              <div class="dragon-list-cell">
+                <label class="dragon-list-label" for="dragon_name_create">Dragon Name: </label>
+                <input class="dragon-list-input" type="text" ref="dragon_name_create" name="dragon_name_create" placeholder="New dragon: name">
+              </div>
+              <div class="dragon-list-cell">
+                <label class="dragon-list-label" for="dragon_type_create">Dragon Type: </label>
+                <input class="dragon-list-input" type="text" ref="dragon_type_create" name="dragon_type_create" placeholder="New dragon: Type">
+              </div>
+              <div class="dragon-list-cell create" v-if="crud">
+                <button class="dragon-list-create" ref="dragon_create" @click="updateDragon(false)">Create Dragon</button>
+              </div>
+            </div>
+
+            <div class="dragon-list-scrollable">
+              <div class="dragon-list-item" v-for="dragon in dragons" :key="dragon.id">
+                <div class="dragon-list-cell">
+                  <label class="dragon-list-label" :for="'dragon_name_edit' + dragon.id">Name: </label>
+                  <input v-if="!crud" class="dragon-list-input" type="text" :value="dragon.name" readonly>
+                  <input v-else class="dragon-list-input" type="text" ref="dragon_name_edit" name="dragon_name_edit" :data-name-edit="dragon.id" :id="'dragon_name_edit' + dragon.id" :value="dragon.name">
+                </div>
+                <div class="dragon-list-cell">
+                  <label class="dragon-list-label" :for="'dragon_type_edit' + dragon.id">Type: </label>
+                  <input v-if="!crud" class="dragon-list-input" type="text" :value="dragon.type" readonly>
+                  <input v-else class="dragon-list-input" type="text" ref="dragon_type_edit" name="dragon_type_edit" :data-type-edit="dragon.id" :id="'dragon_type_edit' + dragon.id" :value="dragon.type">
+                </div>
+                <div class="dragon-list-cell buttons" v-if="crud">
+                  <button class="dragon-list-edit" ref="dragon_edit" @click="updateDragon(dragon.id)">Edit</button>
+                  <button class="dragon-list-delete" ref="dragon_remove" @click="removeDragon(dragon.id)">Remove</button>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'DragonList',
-  props: {
-    dragons: {
-      type: Array,
-      required: true
+  data() {
+    return {
+      dragons: [],
+      loaded: false
     }
+  },
+  props: {
+    crud: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  mounted() {
+    let self = this;
+
+    self.updateDragonList();
+  },
+  methods: {
+    updateDragon(id) {
+      let self = this,
+          data;
+
+      let api = `${self.$parent.api}`,
+          action = 'POST';
+
+      if (id) {
+        api += `${id}`,
+        action = 'PUT',
+        data = {
+          name: self.$el.querySelectorAll('[data-name-edit="' + id +'"]')[0].value,
+          type: self.$el.querySelectorAll('[data-type-edit="' + id +'"]')[0].value
+        };
+      }
+
+      else {
+       data = {
+          name: self.$refs.dragon_name_create.value,
+          type: self.$refs.dragon_type_create.value
+        };
+      }
+
+      fetch(api, {
+        method: action,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }).then((response) => {
+        response.json().then((response) => {
+          self.updateDragonList();
+        })
+      }).catch(err => {
+        console.error(err)
+      });
+    },
+    removeDragon(id) {
+      let self = this;
+
+      fetch(`${self.$parent.api}${id}`, {
+        method: 'DELETE'
+      }).then((response) => {
+        self.updateDragonList();
+      }).catch(err => {
+        console.error(err);
+      });
+    },
+    updateDragonList() {
+      let self = this;
+
+      self.loaded = false;
+
+      fetch(`${self.$parent.api}`)
+        .then((response) => {
+          return response.json();
+        }).then((data) => {
+          self.dragons = data.reverse();
+          self.loaded = true;
+        });
+    },
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: sblock;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+<style lang="scss">
+  @import '../styles/variables';
+  @import '../styles/mixins';
+
+  @import '../styles/components/DragonList';
 </style>
+
